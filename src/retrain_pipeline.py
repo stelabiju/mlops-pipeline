d930ffc8a9b8809707c1cb163fe22b696cd63b81
stelabiju <stelabiju12@gmail.com>
2026-09-15 14:19:47 +0530

    Fix CI tests and update MLflow artifacts

--- src/retrain_pipeline.py
+++ src/retrain_pipeline.py
@@ -6,7 +6,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from monitor_drift import check_data_drift
+from src.monitor_drift import check_data_drift
 
 # Set path to root directory containing mlflow.db
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
